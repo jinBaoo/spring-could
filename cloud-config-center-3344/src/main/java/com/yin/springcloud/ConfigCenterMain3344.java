efbca9c1f5756fa9f195d926316c896cf039fcbd
jinbao.yin <jinbao.yin@partner.bmw-brilliance.cn>
2021-06-17 20:46:40 +0800

    spring cloud

--- cloud-config-center-3344/src/main/java/com/yin/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/yin/springcloud/ConfigCenterMain3344.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.locks.ReentrantLock;
+
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigCenterMain3344
 {
     public static void main(String[] args) {
             SpringApplication.run(ConfigCenterMain3344.class, args);
+            ThreadLocal l = new ThreadLocal();
+//        ThreadPoolExecutor
     }
 }
